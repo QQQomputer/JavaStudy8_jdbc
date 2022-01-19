97cd0cf58c5f45f4593a33510be75f3d6423fe24
MoonJH <3477qq@gmail.com>
2022-01-19 12:41:13 +0900

    start

---
+++ src/com/gom/s1/start/StartMain.java
@@ -0,0 +1,15 @@
+package com.gom.s1.start;
+
+public class StartMain {
+
+	public static void main(String[] args) {		
+		System.out.println("DB 연동 테스트 시작");
+		
+		
+		
+		
+		
+		System.out.println("DB 얀덩 테스트 끝");		
+	}
+
+}
