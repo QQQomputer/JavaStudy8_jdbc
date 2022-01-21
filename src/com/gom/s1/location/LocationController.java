0ac8bb2752378ef1dfa363f47bc95a675fac7f5f
MoonJH <3477qq@gmail.com>
2022-01-22 02:15:16 +0900

    LocalController

---
+++ src/com/gom/s1/location/LocationController.java
@@ -0,0 +1,30 @@
+package com.gom.s1.location;
+
+public class LocationController {
+
+	
+	LocationDAO lDAO = new LocationDAO();
+	LocationView lView = new LocationView();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	System.out.println("1. 부서 리스트 출력");
+	System.out.println("2. 부서 번호  검색");
+	System.out.println("3. 나   가   기 ");
+	
+	
+	
+	
+	
+	
+	
+	
+}
